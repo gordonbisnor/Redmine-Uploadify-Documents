117aa1124edc253cf62bb53f1f4040f31c287b41
Gordon B. Isnor <gordonbisnor@gmail.com>
2011-06-16 17:17:45 -0300

    update for Redmine 1.2 using Rails 2.3.11

--- init.rb
+++ init.rb
@@ -2,9 +2,12 @@
  
 require 'redmine'
 
+config.middleware.insert_before   "ActionController::Session::CookieStore", "FlashSessionCookieMiddleware"
+
 Redmine::Plugin.register :a_redmine_uploadify_documents_plugin do
  name 'Document Uploadify Documents'
  author 'Gordon B. Isnor'
  description 'Uploadify for documents'
- version '0.0.1'
+ version '0.0.2'
 end
+
--- install.rb
+++ install.rb
@@ -1,1 +1,7 @@
-# Install hook code here
+require 'fileutils'
+
+file = '/flash_session_cookie_middleware.rb'
+
+middleware = File.dirname(__FILE__) + '/../../../app/middleware' + file
+
+FileUtils.cp File.dirname(__FILE__) + file, middleware unless File.exist?(middleware)
